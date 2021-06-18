fc19d6217cfc7541993ffeaaaf0254154850784f
Ринат Кирамов <rkiramov@gmail.com>
2021-06-18 13:46:44 +0400

    Окончил работу над заданием 4.9

--- js/main.js
+++ js/main.js
@@ -28,65 +28,52 @@ const getRandomFloatNumberInRange = function (min, max, decimalPlaces) {
   throw new Error('Числа должны быть равны или больше 0');
 };
 
-// Создаю объект author
-let author = {
-  avatar: 'img/avatars/user',
-};
-
-// Сохраняю в переменную вызов функции с параметрами
-const userNumber = getRandomIntegerInRange(1, 10);
-
-// Создаю функцию, которая сохранит в себя number
-const avatarNumber = function () {
-  let stringNumber;
-  if (userNumber < 10) {
-    stringNumber = '0' + userNumber + '.png';
-    return stringNumber;
-  } else {
-    stringNumber = userNumber + '.png';
-    return stringNumber;
-  }
-};
-
-// Обращаемся к свойству объекта и записываем в него результат функции для изменения свойства avatar
-author.avatar += avatarNumber();
-author = author.avatar;
-
-const randomIndexApartments = getRandomIntegerInRange(0, APARTAMENTS.length -1);
-const typeApartments = APARTAMENTS[randomIndexApartments];
-const randomIndexEntry = getRandomIntegerInRange(0, ENTRIES.length -1);
-const checkinEntry = ENTRIES[randomIndexEntry];
-const randomIndexExit = getRandomIntegerInRange(0, ENTRIES.length -1);
-const checkoutExit = ENTRIES[randomIndexExit];
-const randomQuantityFeatures = getRandomIntegerInRange(0, SERVICES.length -1);
-const featuresServices = SERVICES.slice(randomQuantityFeatures);
-const randomQuantityPhotos = getRandomIntegerInRange(0, PHOTOS_APARTMENTS.length -1);
-const photosRandom = PHOTOS_APARTMENTS.slice(randomQuantityPhotos);
-const lat = getRandomFloatNumberInRange(LAT_MIN, LAT_MAX, COORDINATES_DECIMAL_PLACES);
-const lng = getRandomFloatNumberInRange(LNG_MIN, LNG_MAX, COORDINATES_DECIMAL_PLACES);
-
-const coordinates = {
-  lat,
-  lng,
-};
-
-// Создаю объект offer
-const offer = {
-  title: 'Аренда жилья',
-  address: '${coordinates.lat}, ${coordinates.lng}',
-  price: getRandomIntegerInRange(500, 10000),
-  type: typeApartments,
-  rooms: getRandomIntegerInRange(1, 5),
-  guests: getRandomIntegerInRange(1, 5),
-  checkin: checkinEntry,
-  checkout: checkoutExit,
-  features: featuresServices,
-  description: 'У нас самые комфортные условия проживания!',
-  photos: photosRandom,
-};
-
-// Объявляю функцию для размещения объявления
 const createAdvertisement = function () {
+  let author = {
+    avatar: 'img/avatars/user',
+  };
+  const userNumber = getRandomIntegerInRange(1, 10);
+  const avatarNumber = function () {
+    let stringNumber;
+    if (userNumber < 10) {
+      stringNumber = `0${userNumber}.png`;
+      return stringNumber;
+    } else {
+      stringNumber = `${userNumber}.png`;
+      return stringNumber;
+    }
+  };
+  author.avatar += avatarNumber();
+  author = author.avatar;
+  const randomIndexApartments = getRandomIntegerInRange(0, APARTAMENTS.length -1);
+  const typeApartments = APARTAMENTS[randomIndexApartments];
+  const randomIndexEntry = getRandomIntegerInRange(0, ENTRIES.length -1);
+  const checkinEntry = ENTRIES[randomIndexEntry];
+  const randomIndexExit = getRandomIntegerInRange(0, ENTRIES.length -1);
+  const checkoutExit = ENTRIES[randomIndexExit];
+  const randomQuantityFeatures = getRandomIntegerInRange(0, SERVICES.length -1);
+  const featuresServices = SERVICES.slice(randomQuantityFeatures);
+  const randomQuantityPhotos = getRandomIntegerInRange(0, PHOTOS_APARTMENTS.length -1);
+  const photosRandom = PHOTOS_APARTMENTS.slice(randomQuantityPhotos);
+  const lat = getRandomFloatNumberInRange(LAT_MIN, LAT_MAX, COORDINATES_DECIMAL_PLACES);
+  const lng = getRandomFloatNumberInRange(LNG_MIN, LNG_MAX, COORDINATES_DECIMAL_PLACES);
+  const location = {
+    lat,
+    lng,
+  };
+  const offer = {
+    title: 'Аренда жилья',
+    address: `${location.lat}, ${location.lng}`,
+    price: getRandomIntegerInRange(500, 10000),
+    type: typeApartments,
+    rooms: getRandomIntegerInRange(1, 5),
+    guests: getRandomIntegerInRange(1, 5),
+    checkin: checkinEntry,
+    checkout: checkoutExit,
+    features: featuresServices,
+    description: 'У нас самые комфортные условия проживания!',
+    photos: photosRandom,
+  };
   return {
     author,
     offer,
@@ -95,4 +82,4 @@ const createAdvertisement = function () {
 
 const similarAdvertisement = new Array(SIMILAR_ADVERTISEMENT_CONST).fill(null).map(() => createAdvertisement());
 
-console.log(similarAdvertisement);
+similarAdvertisement();
