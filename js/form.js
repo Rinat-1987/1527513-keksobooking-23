7538903625e140217be18808c81ad26d9878fd25
Ринат Кирамов <rkiramov@gmail.com>
2021-06-29 12:02:47 +0400

    Выполнил задание 7.10

--- js/form.js
+++ js/form.js
@@ -1,1 +1,34 @@
 //Здесь будет форма
+
+const adForm = document.querySelector('.ad-form');
+const adFormFieldsets = adForm.querySelectorAll('fieldset');
+const mapForm = document.querySelector('.map__filters');
+const mapFormSelects = mapForm.querySelectorAll('select');
+const mapFormFieldset = mapForm.querySelector('.map__features');
+
+adForm.classList.add('ad-form--disabled');
+for (let index = 0; index < adFormFieldsets.length; index++) {
+  adFormFieldsets[index].setAttribute('disabled', 'disabled');
+}
+
+mapForm.classList.add('map__filters--disabled');
+for (let index = 0; index < mapFormSelects.length; index++) {
+  mapFormSelects[index].setAttribute('disabled', 'disabled');
+}
+mapFormFieldset.setAttribute('disabled', 'disabled');
+
+const activeForm = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapForm.classList.remove('map__filters--disabled');
+  mapFormFieldset.removeAttribute('disabled');
+  for (let index = 0; index < adFormFieldsets.length; index++) {
+    adFormFieldsets[index].removeAttribute('disabled');
+  }
+  for (let index = 0; index < mapFormSelects.length; index++) {
+    mapFormSelects[index].removeAttribute('disabled');
+  }
+};
+
+export {
+  activeForm
+};
--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ import {
   renderPopup
 } from './popup.js';
 
+import {
+  activeForm
+} from './form.js';
+
 const advertisements = createAdvertisements(10);
 const firstAdvertisement = advertisements[0];
 renderPopup(firstAdvertisement);
 
+activeForm();
