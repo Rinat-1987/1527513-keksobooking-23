0610c7802beb857375e495fbe544d3363ed7797b
Ринат Кирамов <rkiramov@gmail.com>
2021-07-03 00:52:44 +0400

    Начал работу над заданием 9.6

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
 import {
-  createAdvertisements
-} from './data.js';
-
-import {
-  renderPopup
-} from './popup.js';
-
-import {
   activateForm
 } from './form.js';
 
+import {
+  map
+} from './map.js';
 
-const advertisements = createAdvertisements(10);
+/*const advertisements = createAdvertisements(10);
 const firstAdvertisement = advertisements[0];
-renderPopup(firstAdvertisement);
+renderPopup(firstAdvertisement);*/
 
 activateForm();
+map;
 
--- js/map.js
+++ js/map.js
@@ -1,1 +1,35 @@
 //Здесь будет карта
+const map = L.map('map-canvas')
+  .on('load', () => {
+  })
+  .setView({
+    lat: 35.6895,
+    lng: 139.692,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: 'img/images leaflet/marker-icon.png',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const marker = L.marker(
+  {
+    lat: 35.6895,
+    lng: 139.692,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+marker.addTo(map);
+
+export {map};
