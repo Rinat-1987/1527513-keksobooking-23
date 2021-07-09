5b9856c9541c093ac9a6478e172c8ef8bb83e328
Ринат Кирамов <rkiramov@gmail.com>
2021-07-10 01:45:39 +0400

    Завершил работу над заданием 9.6

--- js/main.js
+++ js/main.js
@@ -6,21 +6,5 @@ import {
   map
 } from './map.js';
 
-import {
-  createAdvertisements
-} from './data.js';
-
-const newAdvertisement = createAdvertisements(10);
-
-/*const newObject = newAdvertisement.map((obj) => {
-  obj.location;
-});*/
-
-
-newAdvertisement.forEach((obj) => {
-  const newObject = obj.location;
-  console.log(newObject);
-});
-
 activateForm();
 map;
--- js/map.js
+++ js/map.js
@@ -1,4 +1,8 @@
 //Здесь будет карта
+import {
+  createAdvertisements
+} from './data.js';
+
 const map = L.map('map-canvas')
   .on('load', () => {
   })
@@ -15,7 +19,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: 'img/images leaflet/marker-icon.png',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -39,6 +43,55 @@ marker.on('moveend', (evt) => {
   const newMarkerLat = newMarker.lat;
   const newMarkerLng = newMarker.lng;
   address.value = `${newMarkerLat.toFixed(5)}, ${newMarkerLng.toFixed(5)}`;
+});
+
+const createCustomPopup = (point) => {
+  const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+  const popupElement = cardTemplate.cloneNode(true);
+
+  popupElement.querySelector('.popup__avatar').textContent = point.author.avatar;
+  popupElement.querySelector('.popup__title').textContent = point.offer.title;
+  popupElement.querySelector('.popup__text--address').textContent = `Координаты: ${point.offer.address}`;
+  popupElement.querySelector('.popup__text--price').textContent = `Стоимость: ${point.offer.price} ₽/ночь`;
+  popupElement.querySelector('.popup__type').textContent = point.offer.type;
+  popupElement.querySelector('.popup__text--capacity').textContent = `${point.offer.rooms} комнаты для ${point.offer.guests} гостей`;
+  popupElement.querySelector('.popup__text--time').textContent = `Заезд после ${point.offer.checkin}, выезд до ${point.offer.checkout}`;
+  popupElement.querySelector('.popup__features').textContent = point.offer.features;
+  popupElement.querySelector('.popup__description').textContent = point.offer.description;
+  popupElement.querySelector('.popup__photo').src = point.offer.photos;
+
+  if (popupElement.children.textContent === '') {
+    popupElement.children.classList.add('hidden');
+  } else {
+    return popupElement;
+  }
+};
+
+const newAdvertisement = createAdvertisements(10);
+
+newAdvertisement.forEach((point) => {
+  const lat = point.location.lat;
+  const lng = point.location.lng;
+  const icon = L.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+  const newMarker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      icon,
+    },
+  );
+
+  newMarker
+    .addTo(map)
+    .bindPopup(
+      createCustomPopup(point),
+    );
 });
 
 export {map};
