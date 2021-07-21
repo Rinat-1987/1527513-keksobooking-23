8dd5237012b2dbff8b374d215f786f73abf7274f
Ринат Кирамов <rkiramov@gmail.com>
2021-07-21 15:20:48 +0400

    Условия

--- js/filter.js
+++ js/filter.js
@@ -19,12 +19,28 @@ const filterData = (array) => {
       if (selectRooms.value === 'any' || obj.offer.rooms.toString() === selectRooms.value) {
         if (selectGuests.value === 'any' || obj.offer.guests.toString() === selectGuests.value) {
           if (selectPrice.value === 'any') {
+            const findChecked = document.querySelectorAll('input:checked');
+            if (object.offer.features) {
+              if (findChecked.length > 0) {
+                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
             newArray.push(obj);
           } else if (obj.offer.price < FIRST_PRICE_CATEGORY && selectPrice.value === 'low') {
+            const findChecked = document.querySelectorAll('input:checked');
+            if (object.offer.features) {
+              if (findChecked.length > 0) {
+                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
             newArray.push(obj);
           } else if (obj.offer.price >= FIRST_PRICE_CATEGORY && obj.offer.price < SECOND_PRICE_CATEGORY && selectPrice.value === 'middle') {
+            const findChecked = document.querySelectorAll('input:checked');
+            if (object.offer.features) {
+              if (findChecked.length > 0) {
+                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
             newArray.push(obj);
           } else if (obj.offer.price >= SECOND_PRICE_CATEGORY && selectPrice.value === 'high') {
+            const findChecked = document.querySelectorAll('input:checked');
+            if (object.offer.features) {
+              if (findChecked.length > 0) {
+                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
             newArray.push(obj);
           }
         }
