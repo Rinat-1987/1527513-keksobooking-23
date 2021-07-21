f7cc0ca1c5e08aea4d40c8a6d7df668a988a620c
Ринат Кирамов <rkiramov@gmail.com>
2021-07-21 15:45:53 +0400

    Правки

--- js/filter.js
+++ js/filter.js
@@ -14,39 +14,22 @@ const selectGuests = filterForm.querySelector('#housing-guests');
 
 const filterData = (array) => {
   const newArray = [];
-  array.forEach((obj) => {
-    if (selectType.value === 'any' || obj.offer.type === selectType.value) {
-      if (selectRooms.value === 'any' || obj.offer.rooms.toString() === selectRooms.value) {
-        if (selectGuests.value === 'any' || obj.offer.guests.toString() === selectGuests.value) {
+  array.forEach((object) => {
+    if (selectType.value === 'any' || object.offer.type === selectType.value) {
+      if (selectRooms.value === 'any' || object.offer.rooms.toString() === selectRooms.value) {
+        if (selectGuests.value === 'any' || object.offer.guests.toString() === selectGuests.value) {
           if (selectPrice.value === 'any') {
             const findChecked = document.querySelectorAll('input:checked');
+            console.log(object.offer.features);
             if (object.offer.features) {
               if (findChecked.length > 0) {
                 if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
-            newArray.push(obj);
-          } else if (obj.offer.price < FIRST_PRICE_CATEGORY && selectPrice.value === 'low') {
-            const findChecked = document.querySelectorAll('input:checked');
-            if (object.offer.features) {
-              if (findChecked.length > 0) {
-                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
-            newArray.push(obj);
-          } else if (obj.offer.price >= FIRST_PRICE_CATEGORY && obj.offer.price < SECOND_PRICE_CATEGORY && selectPrice.value === 'middle') {
-            const findChecked = document.querySelectorAll('input:checked');
-            if (object.offer.features) {
-              if (findChecked.length > 0) {
-                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
-            newArray.push(obj);
-          } else if (obj.offer.price >= SECOND_PRICE_CATEGORY && selectPrice.value === 'high') {
-            const findChecked = document.querySelectorAll('input:checked');
-            if (object.offer.features) {
-              if (findChecked.length > 0) {
-                if (findChecked.forEach((element) => {object.offer.features.includes(element.value);})) {
-            newArray.push(obj);
+                  newArray.push(obj);}
+              }
+            }
           }
         }
-      }
-    }
-  });
+      }}});
   renderData(newArray);
 };
 
